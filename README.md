6707898d7e54a338511ef37ac58a1263f12b5c29
Luis <lriverawong@outlook.com>
2017-11-22 15:52:46 -0500

    created seed file

--- README.md
+++ README.md
@@ -4,3 +4,19 @@ Resource generator
 ```
 rails g resource Portfolio title:string subtitle:string body:text main_image:text thumb_image:text
 ```
+
+Data for seed
+```
+rails c
+> Blog.create!(title: "asdf", body: "a body")
+```
+
+Runs migrations and sets up database fresh with seeds file
+```
+rails db:setup
+```
+
+At this point mostly looking at this link: 
+```
+http://localhost:3000/pages/home
+```
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: "Commodo consequat ullamco enim do magna in fugiat consequat veniam occaecat. Exercitation occaecat voluptate anim laborum officia. Aliquip ad ullamco incididunt cillum culpa duis voluptate est. Sint ut nisi laboris elit minim cillum incididunt duis dolore nostrud culpa excepteur. Consectetur duis aute labore amet eiusmod non nostrud. Ut nisi laboris sit elit culpa dolore mollit do enim sunt nostrud voluptate excepteur."
+  )
+end
+puts "10 blog posts created"
+
+5.times do |skill|
+  Skill.create!(
+    name: "Rails #{skill}",
+    percent_utilized: 15
+  )
+end
+puts "5 skills created"
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}" ,
+    subtitle: "My great service",
+    body: "Enim mollit incididunt aute Lorem amet.",
+    main_image: "http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/350x150",
+  )
+end
+puts "9 portfolio items created."
